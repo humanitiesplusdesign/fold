7dfede3f1a02f4d1917767dea58ddab7c2c4182c
alexsherman <me.alexsherman@gmail.com>
2016-06-24 16:22:45 -0700

    Update reader.js

--- assets/js/reader.js
+++ assets/js/reader.js
@@ -1,8 +1,23 @@
-// upload word document
-// use docx.js to display this as html || have user save as .html, read, load into div
-// allow user to select section by highlighting a section of the text
-
+//to do: add color and category select, find way to
+// retain proper ordering of elements based on place in text,
+// fix issue with ghost spans preventing re-highlighting
+// fix encoding of apostrophes by microsoft word
 var result;
+var current_category = {
+    "number": 1,
+    "category": "conservative",
+    "color": "red"
+  };
+var current_index = 1;
+var jsonDB = [
+  {
+    "index": 0, //place in the text that the line occurs
+    "text": "", //contents of line-height
+    "category": "", //used to determine colors
+    "color": "", /// unecessary but makes things easier
+    "comment": ""
+  }
+];
 
 document.getElementById("makeSelection").addEventListener("click", function(){makeSelection()});
 document.getElementById("undoSelection").addEventListener("click", function(){undoSelection()});
@@ -60,7 +75,7 @@ function makeSelection() {
     .style("width", oRect.width + "px")
     .style("height", oRect.height + "px");
   d3.select("#text_container").style("opacity", 0);
-  d3.select("#selection").style("opacity", 1);
+  d3.select("#selection").style("opacity", .85);
 
 }
 
@@ -82,13 +97,56 @@ function confirmSelection() {
 }
 
 function newInstructions() {
-  d3.select("#control_container").html("");
+  var highlightScreen = "<p id='instructions3'>Input your categories and choose a corresponding color for each category.</p><br>\
+  <button id ='highlight'>Highlight this</button>"
+  d3.select("#control_container").html(highlightScreen);
 
-  // create array with :
-    // 
+  document.getElementById("highlight").addEventListener("click", function(){highlight()});
+}
+
+function highlight() {
+    var sel = window.getSelection();
+    var selText = sel.toString();
+    var range = sel.getRangeAt(0);
+    var newNode = document.createElement("span");
+    var spanID = current_category.color + current_category.category + current_index;
+    newNode.setAttribute('id', spanID);
+    newNode.setAttribute('style', "background-color: " + current_category.color);
+    newNode.setAttribute('class', "highlightSpan");
+    range.surroundContents(newNode);
+
+    oRange = sel.getRangeAt(0); //get the text range
+    oRect = oRange.getBoundingClientRect();
+
+    d3.select("#text_container").append("div")
+      .attr("class", "sidenote")
+      .attr("id", "sidenote" + current_index)
+      .style("color", current_category.color)
+      .html("<p>" + current_category.category + " " + current_index + "</p>")
+      .style("top", oRect.top + "px");
+
+    var this_index = current_index;
+
+    document.getElementById("sidenote" + current_index).addEventListener("click", function(){
+      removeThisHighlight(this_index, spanID);});
 
 
 
+    var segment = {
+        "index": current_index,
+        "text":  selText, //contents of line-height
+        "category": current_category.category, //used to determine colors
+        "color": current_category.color, /// unecessary but makes things easier
+        "comment": "",
+      };
+    jsonDB.push(segment);
+    current_index += 1;
+    console.log(jsonDB);
+}
 
-
+//fix this
+function removeThisHighlight(index, spanID) {
+    d3.select("#" + spanID).attr("id", "")
+      .style("background-color", "white");
+    d3.select("#sidenote" + index).remove();
 }
